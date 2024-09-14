10bc53b96a30a971155dee86e353dde967a90e9e
bhnone <>
2024-09-15 02:44:11 +0900

    UserRepository

---
+++ src/main/java/redis/data/User.java
@@ -0,0 +1,13 @@
+package redis.data;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+    private String username;
+    private String displayName;
+}
--- src/test/java/com/example/redisspringboot/RedisSpringBootApplicationTests.java
+++ src/test/java/com/example/redisspringboot/RedisSpringBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RedisSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
